f90525440531c6a5c27374a5f4a36f44f6ef5482
alu0101346908 <alu0101346908@ull.edu.es>
2022-12-10 19:02:27 +0000

    JWT and GET tests done, passing

--- backend/routers/get.ts
+++ backend/routers/get.ts
@@ -35,6 +35,7 @@ getRouter.get("/user", (req, res) => {
 getRouter.get("/user/:id", (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
+      console.log(user);
       if (!user) {
         res.status(404).send("The user cannot be found");
       } else {
@@ -42,6 +43,7 @@ getRouter.get("/user/:id", (req, res) => {
       }
     })
     .catch((err) => {
+      console.log(err);
       res.status(400).send(err);
     });
 });
