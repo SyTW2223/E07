5e90b0eb0c94d28afe921ab0e79fd1d9d2236925
alu0101346908 <alu0101346908@ull.edu.es>
2022-12-15 22:03:22 +0000

    Testing GH secrets with actions deploy

--- src/config/env.frontend.ts
+++ src/config/env.frontend.ts
@@ -1,3 +1,3 @@
-export const expressJS_url: string | undefined =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost";
+export const expressJS_url: string | undefined = import.meta.env
+  .VITE_BACKEND_URL;
 export const expressJS_port = import.meta.env.VITE_BACKEND_PORT || 3000;
