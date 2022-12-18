46b90299e74c0f9bacf2bed0b1ae8bb5f3f35b01
alu0101346908 <alu0101346908@ull.edu.es>
2022-12-18 01:36:51 +0000

    ghp

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,9 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "/E07/",
   build: {
-    outDir: './docs'
+    outDir: "./docs",
   },
   test: {
     coverage: {
