66a4cf9fe2851d6d87c793a4447e8e05eed6f529
Mario Hernández García <alu0101346908@ull.edu.es>
2022-12-21 20:25:41 +0000

    Release0.3 (#26)
    * visual changes
    
    * visual changes
    
    * Visual changes (#22)
    
    * Fixed path of public images, added commit message to the gh-pages script
    
    * Removing CD from this branch...
    
    * Added sign up button and commit message for script
    
    * Feature sonarcloud and home component (#23)
    
    * Sonarcloud properties and workflow added, WIP tweet and homepage
    
    * Fixing typo on sonarcloud workflow
    
    * Fixing typo on sonarcloud workflow 2
    
    * Fixing typo on sonarcloud workflow 3
    
    * Fixing typo on sonarcloud workflow 4
    
    * Progress on Homepage
    
    * fix sonarcloud action
    
    Co-authored-by: alu0101326351 <sergiomellado15@gmail.com>

--- src/components/LogInVuetify.vue
+++ src/components/LogInVuetify.vue
@@ -1,7 +1,13 @@
 <template>
+  <v-container>
+    <v-img src="/E07/logo.png" style="width: 15%" class="mx-auto"></v-img>
+  </v-container>
+
   <div>
     <v-form v-model="valid">
-      <v-container>
+      <v-container
+        style="width: 50%; justify-content: center; align-items: center"
+      >
         <v-text-field
           label="Email"
           type="email"
@@ -10,6 +16,9 @@
           v-model="email"
           data-email
           data-test="email-text-box"
+          prepend-icon="mdi-email"
+          
+          style="width: 100%; justify-content: center; align-items: center"
         ></v-text-field>
         <v-text-field
           label="Password"
@@ -19,10 +28,34 @@
           v-model="password"
           data-password
           data-test="password-text-box"
+          prepend-icon="mdi-lock"
+          style="width: 100%; justify-content: center; align-items: center"
         ></v-text-field>
-        <v-container>
-          <v-btn @click="logIn" block color="primary" elevation="2"
+        <v-container
+          style="
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+          "
+        >
+          <v-btn
+            @click="logIn"
+            elevation="2"
+            class="mx-auto"
+            style="display: block; background-color: #0ebbb5; color: white"
+            rounded
             >Log In</v-btn
+          >
+
+          <v-card-text> or if you don't have an account yet </v-card-text>
+
+          <v-btn
+            @click="goToSignUpPage"
+            elevation="2"
+            class="mx-auto"
+            style="display: block; background-color: #1f548b; color: white"
+            rounded
+            >Sign Up</v-btn
           >
         </v-container>
       </v-container>
@@ -32,7 +65,6 @@
 
 <script lang="ts">
 import { useAuthStore } from "@/stores";
-
 export default {
   name: "LogIn",
   data() {
@@ -76,6 +108,9 @@ export default {
         console.log(error);
       }
     },
+    goToSignUpPage() {
+      this.$router.push("/sign-up");
+    }
   },
 };
 </script>
---
+++ src/components/TweetVuetify.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card style="width: 80%; margin: 0 auto; border-radius: 2px">
+    <v-card-title>
+      <v-avatar size="40px">
+        <v-img v-bind:src="tweet.Url"></v-img>
+      </v-avatar>
+      {{ tweet.username }}
+      <span class="date">{{ tweet.date }}</span>
+    </v-card-title>
+    <v-card-text>{{ tweet.text }}</v-card-text>
+    <v-card-actions>
+      <v-btn
+        @click="likeTweet(tweet.id)"
+        :style="{ color: liked ? 'red' : 'grey' }"
+      >
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <span class="num">{{ numLikes }}</span>
+      <v-btn @click="commentOnTweet(tweet.id)" color="blue">
+        <v-icon>mdi-comment</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style>
+.date {
+  font-size: 60%;
+  float: right;
+}
+
+.num {
+  font-size: 90%;
+}
+</style>
+
+<script lang="ts">
+export default {
+  props: ["tweet"],
+  data: () => ({
+    liked: false,
+    numLikes: 0,
+  }),
+
+  created() {
+    this.numLikes = this.tweet.numLikes;
+  },
+
+  methods: {
+    likeTweet(tweetId: any) {
+      this.liked = !this.liked;
+      this.numLikes += this.liked ? 1 : -1;
+
+      // rellenar esto
+    },
+    commentOnTweet(tweetId: any) {
+      // y esto
+    },
+  },
+};
+</script>
